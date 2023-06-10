8c7a12ebd2f1e19ac9d445cf382cd458ac7e7cde
SergMt <66214453+Serg-Mt@users.noreply.github.com>
2023-06-10 13:55:35 +0400

    add NextAuth.js

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,37 @@
+import NextAuth from 'next-auth';
+import CredentialsProvider from 'next-auth/providers/credentials';
+
+export const authOptions = {
+  // Configure one or more authentication providers
+  providers: [
+    CredentialsProvider({
+      // The name to display on the sign in form (e.g. 'Sign in with...')
+      name: 'Credentials',
+      // The credentials is used to generate a suitable form on the sign in page.
+      // You can specify whatever fields you are expecting to be submitted.
+      // e.g. domain, username, password, 2FA token, etc.
+      // You can pass any HTML attribute to the <input> tag through the object.
+      credentials: {
+        username: { label: 'Username', type: 'text', placeholder: 'jsmith' },
+        password: { label: 'Password', type: 'password' }
+      },
+      async authorize(credentials, req) {
+        console.log('credentials', credentials);
+        // Return null if user data could not be retrieved
+
+        if ('1' === credentials.username && '1' === credentials.password)
+          return { id: '1', name: 'J Smith', email: 'jsmith@example.com' };
+        return null;
+      }
+    })
+  ],
+};
+
+const resf = NextAuth(authOptions);
+
+export default (...params) => {
+  const [req] = params;
+  console.log('pages/api/auth/[...nextauth].js ');
+  console.log('>> ', req.method, ' запрос на', req.url, req.query);
+  return resf(...params);
+};
